77c45abc5fa8754667bed0ca814810de10d5f5fb
Riya Garg <96983874+riyaa14@users.noreply.github.com>
2023-03-02 16:47:44 +0530

    Update Abbreviation.cpp

--- Backtracking/Abbreviation.cpp
+++ Backtracking/Abbreviation.cpp
@@ -4,6 +4,8 @@
 #include<string>
 using namespace std;
 
+// **************** APPROACH 1 *********************
+
 void printComb(string str, int code, int i, string out){
     
     if(i>=str.length()) {
@@ -19,12 +21,35 @@ void printComb(string str, int code, int i, string out){
     // j = length of string which is replaced by code
     for(j=1; i+j <= str.length(); j++){
         
-        if(i+j == str.length()) {printComb(str, 1, i+j+1, out + to_string(code)); continue;}
+        if(i+j == str.length()) {
+		printComb(str, 1, i+j+1, out + to_string(code)); 
+		continue;
+	}
         
         printComb(str, 1, i+j+1, out + to_string(code) + str[i+j]);
         
         code++;
     }
+    
+    return;
+}
+
+// *************** APPROACH 2 *********************
+
+void printComb2(string str, int code, int i, string out){
+    
+    if(i>=str.length()) {
+        if(code > 0) out += to_string(code);
+        cout << out << endl;
+        return;
+    }
+    
+    // print letter
+    if(code!=0) printComb2(str, 0, i+1, out + to_string(code) + str[i]);
+    else printComb2(str, 0, i+1, out + str[i]);
+    
+    // print code
+    printComb2(str, code+1, i+1, out);
     
     return;
 }
@@ -35,5 +60,6 @@ int main()
 	string str;
 	cin >> str;
 	printComb(str, 1, 0, "");
+	printComb2(str, 0, 0, "");
 	return 0;
 }
